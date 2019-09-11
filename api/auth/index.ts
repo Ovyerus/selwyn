cc6b2107f16b2f57a77528f6d29a5a836fa7a646
Ovyerus <iamovyerus@gmail.com>
2019-09-11 22:08:55 +1000

    refactor: use JWT for authentication

--- api/auth/index.ts
+++ api/auth/index.ts
@@ -7,6 +7,7 @@ import cookie from 'cookie';
 // only problem is to figure out how to programmatically set config values from script.
 // don't like the idea of using CLI as not everyone who'd want to use this would have that installed.
 import { DbKey, firestore } from '../../lib/db';
+import { createJWT } from '../../lib/jwt';
 import withValidate from '../../lib/withValidate';
 
 const dbKeys = firestore.collection<DbKey>({ path: 'dbkeys' });
@@ -14,26 +15,18 @@ const dbKeys = firestore.collection<DbKey>({ path: 'dbkeys' });
 // eslint-disable-next-line
 const handler = async (req: NowRequest, res: NowResponse) => {
   const { key: mnemonic } = req.body as { key: string };
-  const keys = await dbKeys.fetchAll();
+  const [{ content: key }] = await dbKeys.fetchAll();
 
-  if (
-    // Check if any of the keys match the user's given one.
-    // Ideally there'd only be one key in the DB, but you never know.
-    // XXX: Maybe store dbkey in Firebase functions config
-    !(await Promise.all(
-      keys.map(row => bcrypt.compare(mnemonic, row.content))
-    )).some(x => x)
-  )
+  if (!(await bcrypt.compare(mnemonic, key)))
     return res.status(400).json({ error: 'Invalid auth key' });
 
-  // FIXME: should probably use JWT
-  const encodedKey = Buffer.from(mnemonic).toString('base64');
+  const token = await createJWT();
 
   res.setHeader(
     'Set-Cookie',
-    cookie.serialize('token', encodedKey, { expires: new Date(2147483647000) })
+    cookie.serialize('token', token, { expires: new Date(2147483647000) })
   );
-  res.json({ key: encodedKey });
+  res.json({ key: token });
 };
 
 const schema = Joi.object().keys({ key: Joi.string() });
--- lib/withAuth.ts
+++ lib/withAuth.ts
@@ -1,22 +1,16 @@
 import { NowRequest, NowResponse } from '@now/node';
-import bcrypt from 'bcryptjs';
 
-import { DbKey, firestore } from './db';
+import { compareJWT } from './jwt';
 
 type NowLambda = (req: NowRequest, res: NowResponse) => Promise<any>;
-const dbKeys = firestore.collection<DbKey>({ path: 'dbkeys' });
 
 export default function withAuth(handler: NowLambda): NowLambda {
   return async function(req: NowRequest, res: NowResponse) {
-    const [{ content: key }] = await dbKeys.fetchAll();
-    const userKey = req.cookies.token || (req.headers.authentication as string);
+    const token = req.cookies.token || (req.headers.authentication as string);
 
-    if (!userKey) return res.status(401).send(null);
+    if (!token) return res.status(401).send(null);
 
-    const decoded = Buffer.from(userKey, 'base64').toString();
-
-    if (!(await bcrypt.compare(decoded, key)))
-      return res.status(401).send(null);
+    if (!(await compareJWT(token))) return res.status(401).send(null);
     else return handler(req, res);
   };
 }
