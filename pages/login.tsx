b48c39ce7b5c5e33ec82cf89f326d793544174d0
Ovyerus <iamovyerus@gmail.com>
2021-06-06 21:05:58 +1000

    feat: add dark theme

--- pages/login.tsx
+++ pages/login.tsx
@@ -5,25 +5,17 @@ import React from "react";
 import styles from "~/assets/style/login.module.css";
 
 const LoginPage = () => (
-  <div
-    className={`p-2 flex items-center justify-center min-h-screen ${styles.bg}`}
-  >
-    <main className="w-full p-4 bg-white shadow-md rounded-xl sm:w-max sm:p-6">
-      <a
-        href="/api/auth"
-        className="py-2 px-3 bg-[#181717] text-white flex items-center justify-center text-center rounded-md hover:shadow-lg transition"
-      >
+  <div className="p-2 flex items-center justify-center min-h-screen bg">
+    <main className={styles.card}>
+      <a href="/api/auth" className={styles.ghButton}>
         <Icon icon={github} className="mr-3" />
         Sign in with GitHub
       </a>
 
-      <hr className="mt-4 mb-3 border-gray-300" />
-      <span className="block text-sm text-center w-full -mb-1 sm:-mb-2">
+      <hr className="mt-4 mb-3 border-gray-300 dark:border-gray-700" />
+      <span className={styles.footer}>
         Powered by{" "}
-        <a
-          href="https://github.com/ovyerus/selwyn"
-          className="text-indigo-500 hover:underline"
-        >
+        <a href="https://github.com/ovyerus/selwyn" className="link">
           Selwyn
         </a>
       </span>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,17 @@
+const colors = require("tailwindcss/colors");
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   mode: "jit",
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: "media",
   theme: {
     extend: {
       fontFamily: {
         sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        gray: { ...colors.gray, 900: "#131516", 800: "#1D1F20" },
       },
     },
     fill: {
