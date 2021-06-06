0001954f162d85d6be07ec5412e75320f095ca6d
Ovyerus <iamovyerus@gmail.com>
2021-06-06 21:06:29 +1000

    feat: add animations & timeouts to toasts

--- components/Toast.tsx
+++ components/Toast.tsx
@@ -1,26 +1,52 @@
 import close from "@iconify/icons-gg/close";
 import { Icon } from "@iconify/react";
 import cc from "classcat";
-import React from "react";
+import { motion } from "framer-motion";
+import React, { useEffect } from "react";
 
-const Toast = ({ children, className, onClose }: ToastProps) => (
-  <div
-    role="status"
-    className={cc([
-      "bg-white sm:max-w-sm w-full p-4 flex items-center leading-tight rounded-xl shadow-xl",
-      className,
-    ])}
-  >
-    <div className="w-full flex-grow">{children}</div>
-    <button className="rounded-button" onClick={() => onClose()}>
-      <Icon icon={close} />
-    </button>
-  </div>
-);
+const Toast = ({ children, className, duration, onClose }: ToastProps) => {
+  // TODO: useReducedMotion
+
+  useEffect(() => {
+    if (!duration) return;
+
+    const timeout = setTimeout(() => onClose(), duration);
+    return () => clearTimeout(timeout);
+  }, []);
+
+  return (
+    <motion.div
+      role="status"
+      className={cc([
+        "bg-white dark:bg-black dark:text-white sm:max-w-sm w-full p-4 flex items-center leading-tight rounded-xl shadow-xl",
+        className,
+      ])}
+      // Can't use percentage values here, otherwise we get a layout jump :s
+      // https://github.com/framer/motion/issues/648
+      initial={{ opacity: 0, x: 500 }}
+      animate={{ opacity: 1, x: 0 }}
+      exit={{ opacity: 0, x: 500 }}
+      transition={{
+        type: "spring",
+        mass: 0.25,
+        damping: 10,
+        stiffness: 100,
+        velocity: 2,
+      }}
+      layout
+    >
+      <div className="w-full flex-grow">{children}</div>
+      <button className="rounded-button" onClick={() => onClose()}>
+        <Icon icon={close} />
+      </button>
+    </motion.div>
+  );
+};
 
 export interface ToastProps {
   children: React.ReactNode;
   className?: string;
+  duration: number;
   onClose(): void;
 }
 
--- components/Toasts.tsx
+++ components/Toasts.tsx
@@ -1,3 +1,4 @@
+import { AnimatePresence } from "framer-motion";
 import React from "react";
 import ReactDOM from "react-dom";
 import shallow from "zustand/shallow";
@@ -15,18 +16,19 @@ const Toasts = () => {
   if (typeof window === "undefined") return null;
   const container = document.getElementById("overlays-root")!;
 
-  // TODO: timeouts to auto-remove, and transitions.
   return ReactDOM.createPortal(
     <div className="fixed w-screen h-screen top-0 left-0 p-3 flex items-end justify-end pointer-events-none">
       <div
         role="log"
-        className="w-full items-end flex flex-col gap-4 pointer-events-auto"
+        className="items-end flex flex-col gap-4 pointer-events-auto sm:max-w-sm w-full overflow-visible"
       >
-        {toasts.map(({ id, className, children }) => (
-          <Toast key={id} className={className} onClose={() => popToast(id)}>
-            {children}
-          </Toast>
-        ))}
+        <AnimatePresence initial={false}>
+          {toasts.map(({ id, children, ...props }) => (
+            <Toast key={id} onClose={() => popToast(id)} {...props}>
+              {children}
+            </Toast>
+          ))}
+        </AnimatePresence>
       </div>
     </div>,
     container
